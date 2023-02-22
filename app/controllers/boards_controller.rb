a49171bab821b8a851ac0675d1924121add4d62d
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-02-22 19:01:14 +0900

    feat: 掲示板作成機能を追加するために、createアクション追加・「掲示板を作成しました」のフラッシュメッセージ追加・掲示板作成失敗時の「掲示板を作成できませんでした」メッセージを追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,26 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
   end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    #@board = Board.new(board_params)
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      #[, item: Board.model_name.human]を追加
+      redirect_to(boards_path, flash:{success: t('.success', item: Board.model_name.human)})
+    else
+      flash.now[:danger] = t('.fail', item: Board.model_name.human)
+      render "new"
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body)
+  end
 end
